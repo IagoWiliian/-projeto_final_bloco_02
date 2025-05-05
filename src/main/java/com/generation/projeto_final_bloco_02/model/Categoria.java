2ed71f256e2673cd7ea35e90c8070b9a47968792
ÃIago Wilian <agowillian22@gmail.com>
2025-05-05 11:27:26 -0300

    construcao da segunda tabela e relacionamento

--- src/main/java/com/generation/projeto_final_bloco_02/model/Categoria.java
+++ src/main/java/com/generation/projeto_final_bloco_02/model/Categoria.java
@@ -1,11 +1,20 @@
 package com.generation.projeto_final_bloco_02.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "tb_categoria")
@@ -16,7 +25,12 @@ public class Categoria {
 	private Long id;
 
 	@NotNull(message = "O Atributo Descrição é obrigatório")
+	@Size(min = 3, max = 200)
 	private String descricao;
+
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produtos;
 
 	public Long getId() {
 		return this.id;
@@ -34,4 +48,11 @@ public class Categoria {
 		this.descricao = descricao;
 	}
 
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
 }
---
+++ src/main/java/com/generation/projeto_final_bloco_02/model/Produto.java
@@ -0,0 +1,75 @@
+package com.generation.projeto_final_bloco_02.model;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank
+	@Size(min = 5, max = 100)
+	private String nome;
+	
+	
+	@NotNull
+	private LocalDate datavalidade;
+	
+
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public LocalDate getDatafabricacao() {
+		return datavalidade;
+	}
+
+	public void setDatafabricacao(LocalDate datavalidade) {
+		this.datavalidade = datavalidade;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+
+
+
+}
